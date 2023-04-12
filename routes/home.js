cef4d82c52709d77a9eebad1405c5d4dc0985b30
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2023-04-12 11:27:53 +0200

    added prompt page and functionality

--- routes/home.js
+++ routes/home.js
@@ -5,5 +5,9 @@ router.get("/", async (req, res) => {
   res.render("home");
 });
 
+router.get("/prompt", async (req, res) => {
+  res.render("prompt");
+});
+
 // Export the router
 module.exports = router;
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,43 @@
-console.log("index.js loaded");
+const poemForm = document.querySelector("#poemForm");
+const typeSelect = document.querySelector("#poemForm select");
+const themeInput = document.querySelector("#poemForm input");
+const output = document.querySelector(".output");
+
+poemForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+  fetchPoem();
+});
+
+ async function fetchPoem() {
+  const type = typeSelect.value;
+  const theme = themeInput.value;
+  const url = `https://dichter.responsible-it.nl/api/poetry?type=${type}&theme=${encodeURIComponent(theme)}`;
+  const headers = {
+    "Authorization": `Bearer dichter-a6a8ae46-4c90`  };
+
+  try {
+    const response = await fetch(url, { headers });
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+    const data = await response.json();
+    output.textContent = data.data.paragraph;
+  } catch (error) {
+    console.error("Er is een fout opgetreden:", error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ulElement = document.querySelector('header ul');
+  const inputField = document.getElementById('theme');
+  inputField.addEventListener('input', (event) => {
+    const theme = event.target.value;
+    const themes = theme.split(',').map((theme) => theme.trim());
+    ulElement.innerHTML = '';
+    themes.forEach((theme) => {
+      const liElement = document.createElement('li');
+      liElement.innerText = theme;
+      ulElement.appendChild(liElement);
+    });
+  });
+});
